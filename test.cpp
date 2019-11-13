46b5f749e79c529f1bf8a87bc1b1cb514ef09f08
Francois Balducchi <fbalduc@a-139160.ups.u-psud.fr>
2019-11-13 16:07:06 +0100

    Load data

--- test.cpp
+++ test.cpp
@@ -32,3 +32,41 @@ void readData(){
     String Team;
     getline(
     
+	void loadData(string fn, vector<int> &pts, vector<int> &ranks)	
+    	{
+    	ifstream file ( "../data/rankpts.csv" ); // declare file stream: http://www.cplusplus.com/reference/iostream/ifstream/
+    	int pts [20][38];
+    	int rk [20][38];
+    	String nomTeam[20];
+    	for(int i = 0; i < nbequipe; i++)
+    {
+        /*string value;
+        getline ( file, value, '\n' );
+        int nbteam = nbteam;
+        */
+        String Team;
+        getline(file, Team, ',');
+        nomTeam[i] = Team
+        int tabres [38][6]; //38j, 6 entrees par jour
+        int j = 0;
+        while(file.good())
+        {
+             getline ( file, value, ',' );
+             string ranko = string( value, 0, value.length() );
+             getline ( file, value, ',' );
+             string ptso = string( value, 0, value.length() );
+             getline ( file, value, ',' );
+             string villej1 = string( value, 0, value.length() );
+             getline ( file, value, ',' );
+             string scorej1 = string( value, 0, value.length() );
+             //float lon = stof(slon, NULL);
+             getline ( file, value, ',' );
+             string scorej2 = string( value, 0, value.length() );
+             //float lat = stof(slat, NULL);
+             getline( file, value, ',' );
+             string villej2 = string( value, 0, value.length() );
+        }
+
+    }
+}
+
