0875814f8b02122bb11fcac2a5028eabdc83bf97
Francois Balducchi <fbalduc@a-139160.ups.u-psud.fr>
2019-11-20 16:10:13 +0100

    Test fr avec main qui marche

--- test2.cpp
+++ test2.cpp
@@ -30,34 +30,6 @@ using namespace std;
 
 using namespace glm;
 
-void readData(){
-  ifstream file ( "../data/rankpts.csv" ); // declare file stream: http://www.cplusplus.com/reference/iostream/ifstream/
-        string value;
-        getline ( file, value, '\n' );
-        while ( file.good() ) {
-
-             getline ( file, value, ',' ); // read a string until next comma: http://www.cplusplus.com/reference/string/getline/
-             string villedep = string( value, 0, value.length() );
-             getline ( file, value, ',' );
-             string ranko = string( value, 0, value.length() );
-             getline ( file, value, ',' );
-             string ptso = string( value, 0, value.length() );
-             getline ( file, value, ',' );
-             string villej1 = string( value, 0, value.length() );
-             getline ( file, value, ',' );
-             string scorej1 = string( value, 0, value.length() );
-             //float lon = stof(slon, NULL);
-             getline ( file, value, ',' );
-             string scorej2 = string( value, 0, value.length() );
-             //float lat = stof(slat, NULL);
-             getline ( file, value, ',' );
-             string villej2 = string( value, 0, value.length() );
-             /*getline ( file, value, '\n' );
-             string ssur = string( value, 0, value.length() );
-*/
-             //cout << lon << " ... " << lat << endl; // display value
-        }
-}
 string DeathToSpace(string a)
 {
     int i = 0;
@@ -79,66 +51,38 @@ void loadData(string fn, vector<int> &pts, vector<int> &ranks)
     	int points [20][38];
     	int rk [20][38];
     	int nbequipe = 20;
+        int nbj = 41;
     	vector<string> nomTeam;
-
-    	for(int i = 0; i < nbequipe; i++)
-    {
-        string line;
         string value;
-        cout << "1st" << endl;
-        getline ( file, value, '\n' );
-        cout << "2nd" << endl;
-        //nomTeam[i]= string(value,0,value.length());
-        string TeamBuff;
-        getline(file, TeamBuff, ',');
-        cout << "3rd" << endl;
-        nomTeam.push_back(TeamBuff);
-        cout << nomTeam[i] << endl;
-        string tabres [38][6]; //38j, 6 entrees par jour
-        int j = 0;
-        while(file.good())
+    	for(int i = 0; i < nbequipe*nbj ; i++)
         {
+             if (i % 41 == 0)
+            {
              getline ( file, value, ',' );
-                cout << value <<endl;
-             ranks.push_back(stoi(DeathToSpace((string( value, 0, value.length() )))));
-             //tabres[i][0]=ranko;
-                       cout << "4rd" << endl;
-             getline ( file, value, ',' );
-             pts.push_back(stoi(DeathToSpace(string( value, 0, value.length() ))));
-             //tabres[i][1]=ptso;
-        cout << "5rd" << endl;
-             getline ( file, value, ',' );
-             string villej1 = string( value, 0, value.length() );
-             tabres[i][2]=villej1;
-        cout << "6rd" << endl;
-             getline ( file, value, ',' );
-             string scorej1 = string( value, 0, value.length() );
-             //float lon = stof(slon, NULL);
-             tabres[i][3]=scorej1;
-        cout << "7rd" << endl;
-             getline ( file, value, ',' );
-             string scorej2 = string( value, 0, value.length() );
-             //float lat = stof(slat, NULL);
-             tabres[i][4]=scorej2;
-        cout << "8rd" << endl;
+             nomTeam.push_back(value);
+            }
              getline( file, value, ',' );
-             string villej2 = string( value, 0, value.length() );
-             tabres[i][5]=villej2;
-        cout << "9rd" << endl;
+             ranks.push_back(stoi(DeathToSpace((string( value, 0, value.length() )))));
+             getline( file, value, ',' );
+             pts.push_back(stoi(DeathToSpace(string( value, 0, value.length() ))));
+             getline( file, value, ',' );
+             getline( file, value, ',' );
+             getline( file, value, ',' );
+             getline( file, value, ',' );
+            if (i % 41 == 40)
+             getline(file, value, '\n');     
         }
-
-    }
 }
 
 int main(void)
 {
 	int j = 0;
-	vector<int> pts(20);
-	vector<int> ranks(20);
+	vector<int> pts;
+	vector<int> ranks;
 	loadData("data/rankspts.csv",pts,ranks);
 	cout << pts.size()<<endl;
 	cout << ranks.size()<<endl;
-	while(j < 20)
+	while(j < pts.size())
 	{
 		cout << pts[j] << endl;
 		cout << ranks[j] << endl;
